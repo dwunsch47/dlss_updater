7dd1159de44e541260bf1d7827f94351b899079b
Anton <csengerhk@yandex.ru>
2024-10-18 05:58:23 +0700

    Support specific config file path and various types of submitted paths
    Supported types of submitted paths:
        direct path to nvngx_dlss.dll
        path to directory with nvngx_dlss.dll
        path to "game library", directory which holds many game directories,
        with recurcive search in them

--- path_storage.cpp
+++ path_storage.cpp
@@ -16,6 +16,10 @@ PathStorage::PathStorage() {
 	restoreSavedFilePaths();
 }
 
+PathStorage::PathStorage(filesystem::path path_to_config_dir) : config_dir_location_(filesystem::is_directory(path_to_config_dir) ? path_to_config_dir : path_to_config_dir.parent_path()) {
+	restoreSavedFilePaths();
+}
+
 PathStorage::~PathStorage() {
 	saveFilePaths();
 }
@@ -26,44 +30,72 @@ void PathStorage::AddNewPaths(const vector<filesystem::path>& new_paths) {
 	}
 
 	is_changed = true;
-	for (const filesystem::path& file_path : new_paths) {
+	/*for (const filesystem::path& file_path : new_paths) {
 		stored_path_to_recency.emplace(file_path, true);
+	}*/
+	for (filesystem::path file_path : new_paths) {
+		if (!filesystem::exists(file_path) && !filesystem::exists(file_path.parent_path()) && (file_path == filesystem::current_path())) {
+			continue;
+		}
+
+		if (filesystem::is_regular_file(file_path)) {
+			file_path = file_path.parent_path();
+		}
+		else if (!filesystem::is_directory(file_path)) {
+			throw runtime_error("given path is neither a directory, nor a regular file: " + file_path.string());
+		}
+
+		if (filesystem::exists(file_path / DLSS_DLL_NAME)) {
+			stored_path_to_recency_.emplace(file_path, true);
+		}
+		else {
+			for (const filesystem::path& true_file_path : filesystem::recursive_directory_iterator(file_path)) {
+				if (filesystem::is_regular_file(true_file_path) && true_file_path.filename() == DLSS_DLL_NAME) {
+					stored_path_to_recency_.emplace(true_file_path.parent_path(), true);
+				}
+			}
+		}
 	}
 
 #if _DEBUG
-	cout << "It added new paths. stored_paths size is " << stored_path_to_recency.size() << endl;
+	cout << "It added new paths. stored_paths size is " << stored_path_to_recency_.size() << endl;
+	cout << "Displaying contents of stored_path_to_recency_:";
+	for (const auto& [curr_disp_path, recency] : stored_path_to_recency_) {
+		cout << ' ' << curr_disp_path.string();
+	}
+	cout << endl;
 #endif
 }
 
 const map<filesystem::path, bool>& PathStorage::GetStoredPaths() const {
-	return stored_path_to_recency;
+	return stored_path_to_recency_;
 }
 
 void PathStorage::restoreSavedFilePaths() {
-	if (!filesystem::exists(PATH_STORAGE_FILENAME)) {
+	if (!filesystem::exists(config_dir_location_ / PATH_STORAGE_FILENAME)) {
 		return;
 	}
-	fstream path_storage_file(PATH_STORAGE_FILENAME, ios::in);
+	fstream path_storage_file(config_dir_location_ / PATH_STORAGE_FILENAME, ios::in);
 
 	string file_path;
 
 	while (getline(path_storage_file, file_path)) {
-		stored_path_to_recency.emplace(file_path, false);
+		stored_path_to_recency_.emplace(file_path, false);
 	}
 #if _DEBUG
-	cout << "We succesfully restored paths. Current number of paths is " << stored_path_to_recency.size() << endl;
+	cout << "We succesfully restored paths. Current number of paths is " << stored_path_to_recency_.size() << endl;
 #endif
 }
 
 void PathStorage::saveFilePaths() const {
-	if (stored_path_to_recency.empty() || !is_changed) {
+	if (stored_path_to_recency_.empty() || !is_changed) {
 		return;
 	}
 	
-	fstream storage_file(PATH_STORAGE_FILENAME, ios::out | ios::app);
+	fstream storage_file(config_dir_location_ / PATH_STORAGE_FILENAME, ios::out | ios::app);
 	
 	if (storage_file.good()) {
-		for (const auto& [file_path, is_recent] : stored_path_to_recency) {
+		for (const auto& [file_path, is_recent] : stored_path_to_recency_) {
 			if (is_recent) {
 				storage_file << file_path.string() << '\n';
 			}
--- path_storage.h
+++ path_storage.h
@@ -7,18 +7,21 @@
 #include <string>
 
 const std::string PATH_STORAGE_FILENAME = "locations.txt";
+const std::string DLSS_DLL_NAME = "nvngx_dlss.dll";
 
 class PathStorage {
 public:
 	explicit PathStorage();
+	explicit PathStorage(std::filesystem::path path_to_config_dir);
 
 	~PathStorage();
 
 	void AddNewPaths(const std::vector<std::filesystem::path>& new_paths);
 	const std::map<std::filesystem::path, bool>& GetStoredPaths() const;
 private:
-	std::map<std::filesystem::path, bool> stored_path_to_recency;
+	std::map<std::filesystem::path, bool> stored_path_to_recency_;
 	bool is_changed = false;
+	const std::filesystem::path config_dir_location_ = std::filesystem::current_path();
 
 	void restoreSavedFilePaths();
 	void saveFilePaths() const;
