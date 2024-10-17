464c8646b4dfec975059b48e06b7230bfa771d6f
Anton <csengerhk@yandex.ru>
2024-10-18 05:57:03 +0700

    Correct DLSS dll name

--- file_copy.cpp
+++ file_copy.cpp
@@ -10,6 +10,6 @@ void fileCopy(const map<filesystem::path, bool>& paths) {
 	const auto copyOptions = filesystem::copy_options::overwrite_existing;
 
 	for (const auto& [file_path, recency] : paths) {
-		filesystem::copy("nvngx.dll", file_path, copyOptions);
+		filesystem::copy("nvngx_dlss.dll", file_path, copyOptions);
 	}
 }
