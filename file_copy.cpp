79d4242d1b26b9bace9b962ec49e945cfce66ed1
Anton <csengerhk@yandex.ru>
2024-10-21 01:54:27 +0700

    file_copy: support new path storage format

--- file_copy.cpp
+++ file_copy.cpp
@@ -1,15 +1,16 @@
 #include "file_copy.h"
 
 #include <filesystem>
+#include <tuple>
 #include <map>
 
 using namespace std;
 
-void fileCopy(const map<filesystem::path, bool>& paths) {
+void fileCopy(const map<filesystem::path, std::tuple<bool, std::string>>& paths) {
 
 	const auto copyOptions = filesystem::copy_options::overwrite_existing;
 
-	for (const auto& [file_path, recency] : paths) {
+	for (const auto& [file_path, info] : paths) {
 		filesystem::copy("nvngx_dlss.dll", file_path, copyOptions);
 	}
 }
--- file_copy.h
+++ file_copy.h
@@ -2,9 +2,11 @@
 
 
 #include <filesystem>
+#include <string>
+#include <tuple>
 #include <map>
 
-void fileCopy(const std::map<std::filesystem::path, bool>& paths);
+void fileCopy(const std::map<std::filesystem::path, std::tuple<bool, std::string>>& paths);
 
 
 
