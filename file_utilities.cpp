c2532fc73a34b877b44f3b83f238e91eb83d9a0d
Anton <csengerhk@yandex.ru>
2024-10-21 01:52:45 +0700

    Add separate namespace for file_utilities

--- file_utilities.cpp
+++ file_utilities.cpp
@@ -3,27 +3,30 @@
 #include <Windows.h>
 #include <filesystem>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
-string GetDLLVersion(const filesystem::path& file_path) {
-	DWORD dwHandle, dwLen = GetFileVersionInfoSizeA(file_path.string().c_str(), &dwHandle);
-	if (dwLen == 0) {
-		return "error";
-	}
+namespace fileUtil {
+	string GetDLLVersion(const filesystem::path& file_path) {
+		DWORD dwHandle, dwLen = GetFileVersionInfoSizeA(file_path.string().c_str(), &dwHandle);
+		if (dwLen == 0) {
+			return "error";
+		}
 
-	unique_ptr<char> buf(new char[dwLen]);
-	if (!GetFileVersionInfoA(file_path.string().c_str(), dwHandle, dwLen, buf.get())) {
-		return "error";
-	}
+		unique_ptr<char> buf(new char[dwLen]);
+		if (!GetFileVersionInfoA(file_path.string().c_str(), dwHandle, dwLen, buf.get())) {
+			return "error";
+		}
 
-	VS_FIXEDFILEINFO* fileinfo = NULL;
-	UINT len = 0;
-	if (!VerQueryValueA(buf.get(), "\\", (LPVOID *)&fileinfo, &len)) {
-		return "error";
-	}
+		VS_FIXEDFILEINFO* fileinfo = NULL;
+		UINT len = 0;
+		if (!VerQueryValueA(buf.get(), "\\", (LPVOID*)&fileinfo, &len)) {
+			return "error";
+		}
 
-	ostringstream output;
-	output << HIWORD(fileinfo->dwFileVersionMS) << '.' << LOWORD(fileinfo->dwFileVersionMS) << '.' << HIWORD(fileinfo->dwFileVersionLS) << '.' << LOWORD(fileinfo->dwFileVersionLS);
-	return output.str();
+		ostringstream output;
+		output << HIWORD(fileinfo->dwFileVersionMS) << '.' << LOWORD(fileinfo->dwFileVersionMS) << '.' << HIWORD(fileinfo->dwFileVersionLS) << '.' << LOWORD(fileinfo->dwFileVersionLS);
+		return output.str();
+	}
 }
--- file_utilities.h
+++ file_utilities.h
@@ -3,5 +3,6 @@
 #include <filesystem>
 #include <string>
 
-std::string GetDLLVersion(const std::filesystem::path& file_path);
-
+namespace fileUtil {
+	std::string GetDLLVersion(const std::filesystem::path& file_path);
+}
