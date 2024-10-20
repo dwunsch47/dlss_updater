a5e4b6a182be4497fefb59318cb8b642209207a8
Anton <csengerhk@yandex.ru>
2024-10-21 00:43:11 +0700

    Remove some more commented lines

--- file_utilities.h
+++ file_utilities.h
@@ -3,6 +3,5 @@
 #include <filesystem>
 #include <string>
 
-//int GetDLLVersion(const std::filesystem::path& file_path, std::shared_ptr<char[]> version_info);
 std::string GetDLLVersion(const std::filesystem::path& file_path);
 
