e3ba65b383de25db2508ecf672ccd6738cc6641f
Anton <csengerhk@yandex.ru>
2024-10-22 03:51:05 +0700

    tests: update tests for new class behavious

--- tests.cpp
+++ tests.cpp
@@ -7,39 +7,45 @@
 #include <fstream>
 #include <iostream>
 #include <cassert>
+#include <chrono>
 
 using namespace std;
 
 void TestPathStorage() {
-	const filesystem::path root_working_dir = filesystem::current_path();
-	filesystem::path current_working_dir = root_working_dir / "path_storage_test";
+	if (filesystem::exists(PATH_STORAGE_FILENAME)) {
+		filesystem::rename(PATH_STORAGE_FILENAME, (PATH_STORAGE_FILENAME + ".orig"));
+	}
+	if (filesystem::exists(DLSS_DLL_NAME)) {
+		filesystem::rename(DLSS_DLL_NAME, (DLSS_DLL_NAME + ".orig"));
+	}
+	const filesystem::path root_dir = filesystem::current_path();
+	const filesystem::path current_working_dir = root_dir / "test_dir";
 	filesystem::create_directory(current_working_dir);
-	cout << "Current working directory is : " << current_working_dir << endl;
 
 	{
-		filesystem::remove(current_working_dir / PATH_STORAGE_FILENAME);
+		filesystem::remove(PATH_STORAGE_FILENAME);
 		cout << "FIRST TEST" << endl;
 
-		const filesystem::path test_dir_path = current_working_dir / "first_dir";
-		filesystem::create_directory(test_dir_path);
-		fstream first_file(test_dir_path / DLSS_DLL_NAME, ios::out);
-		vector<filesystem::path> test_paths = { test_dir_path };
+		filesystem::path test_dir = current_working_dir / "testdir1";
+		filesystem::create_directory(test_dir);
+		fstream first_file(test_dir / DLSS_DLL_NAME, ios::out);
+		vector<filesystem::path> test_paths = { test_dir };
 
-		PathStorage test_storage(current_working_dir);
+		PathStorage test_storage(test_dir);
 		test_storage.AddNewPaths(test_paths);
 		assert(test_storage.GetStoredPaths().size() == test_paths.size());
 	}
 
 	{
-		filesystem::remove(current_working_dir / PATH_STORAGE_FILENAME);
+		filesystem::remove(PATH_STORAGE_FILENAME);
 		cout << "SECOND TEST" << endl;
 
-		const filesystem::path test_dir_path = current_working_dir / "second_dir";
-		filesystem::create_directory(test_dir_path);
-		fstream first_file(test_dir_path / DLSS_DLL_NAME, ios::out);
-		vector<filesystem::path> test_paths = { test_dir_path, test_dir_path / DLSS_DLL_NAME };
+		const filesystem::path test_dir = current_working_dir / "testdir2";
+		filesystem::create_directory(test_dir);
+		fstream first_file(test_dir / DLSS_DLL_NAME, ios::out);
+		vector<filesystem::path> test_paths = { test_dir, test_dir / DLSS_DLL_NAME };
 
-		PathStorage test_storage(current_working_dir);
+		PathStorage test_storage(test_dir);
 		test_storage.AddNewPaths(test_paths);
 		assert(test_storage.GetStoredPaths().size() == 1);
 	}
@@ -60,5 +66,13 @@ void TestPathStorage() {
 		cout << "RESULT: " << get<0>(test1) << '.' << get<1>(test1) << '.' << get<2>(test1) << '.' << get<3>(test1) << endl;
 		assert(test1_result == test1);
 	}
+	
 	filesystem::remove_all(current_working_dir);
+
+	if (filesystem::exists(PATH_STORAGE_FILENAME + ".orig")) {
+		filesystem::rename((PATH_STORAGE_FILENAME + ".orig"), PATH_STORAGE_FILENAME);
+	}
+	if (filesystem::exists(DLSS_DLL_NAME + ".orig")) {
+		filesystem::rename(DLSS_DLL_NAME + ".orig", DLSS_DLL_NAME);
+	}
 }
