9771b08de77cfac21ac29b7b635430084a01ebb2
Anton <csengerhk@yandex.ru>
2024-10-22 01:12:17 +0700

    file_copy: add initial copy error handling

--- file_copy.cpp
+++ file_copy.cpp
@@ -3,6 +3,7 @@
 
 #include <filesystem>
 #include <algorithm>
+#include <iostream>
 #include <string>
 #include <tuple>
 #include <map>
@@ -14,13 +15,17 @@ void fileCopy(const map<filesystem::path, std::tuple<bool, std::string>>& paths,
 	const uintmax_t dll_size = filesystem::file_size(dll_path / DLSS_DLL_NAME);
 
 	const auto copyOptions = filesystem::copy_options::overwrite_existing;
+	error_code copy_ec;
 
 	tuple<int, int, int, int> file_path_version = { 0, 0, 0, 0 };
 	for (const auto& [file_path, info] : paths) {
 		file_path_version = fileUtil::formatDLLVersion(fileUtil::getDLLVersion(file_path / DLSS_DLL_NAME));
 
 		if (dll_version > file_path_version || (dll_version == file_path_version && dll_size != filesystem::file_size(file_path / DLSS_DLL_NAME))) {
-			filesystem::copy(DLSS_DLL_NAME, file_path, copyOptions);
+			filesystem::copy(DLSS_DLL_NAME, file_path, copyOptions, copy_ec);
+			if (copy_ec) {
+				cerr << "An error during copy occurred. Error info: " << copy_ec.message() << endl;
+			}
 		}
 	}
 }
